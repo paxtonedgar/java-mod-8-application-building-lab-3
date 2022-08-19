b18a8f38aca9f6442ee36f51925f28779b5aa60b
Paxton Edgar <paxtonedgar3@gmail.com>
2022-08-19 15:48:59 -0400

    init push

--- messengerApp/src/app/conversation-thread/conversation-thread.component.ts
+++ messengerApp/src/app/conversation-thread/conversation-thread.component.ts
@@ -7,7 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./conversation-thread.component.css']
 })
 export class ConversationThreadComponent implements OnInit {
+  senderMessages = [
+    {
+      sender: { firstName: "Ludovic" },
+      text: "Message from Ludovic",
+      conversationId: 1,
+      sequenceNumber: 0,
+    },
+    {
+      sender: { firstName: "Jessica" },
+      text: "Message from Jessica",
+      conversationId: 1,
+      sequenceNumber: 1,
+    },
+  ];
 
+  userMessages = [
+    {
+      sender: { firstName: "Aurelie" },
+      text: "Message from Aurelie",
+      conversationId: 1,
+      sequenceNumber: 2,
+    },
+  ];
   constructor() { }
 
   ngOnInit(): void {
--- messengerApp/src/app/user-message/user-message.component.ts
+++ messengerApp/src/app/user-message/user-message.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user-message',
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-message.component.css']
 })
 export class UserMessageComponent implements OnInit {
+
+  @Input() message = {
+    sender: { firstName: "Ludovic" },
+    text: "Message from Ludovic",
+    conversationId: 1,
+    sequenceNumber: 0,
+  };
 
   constructor() { }
 
