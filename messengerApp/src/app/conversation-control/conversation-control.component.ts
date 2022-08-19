f21176b51d488fd3d1c183bde40ea8945906efd7
Paxton Edgar <paxtonedgar3@gmail.com>
2022-08-19 15:07:50 -0400

    init push

---
+++ messengerApp/src/app/conversation-control/conversation-control.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-conversation-control',
+  templateUrl: './conversation-control.component.html',
+  styleUrls: ['./conversation-control.component.css']
+})
+export class ConversationControlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ messengerApp/src/app/conversation-thread/conversation-thread.component.ts
@@ -0,0 +1,16 @@
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-conversation-thread',
+  templateUrl: './conversation-thread.component.html',
+  styleUrls: ['./conversation-thread.component.css']
+})
+export class ConversationThreadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
